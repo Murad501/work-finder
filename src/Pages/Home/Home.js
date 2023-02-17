d595b056fb1ca554b66cabf71a54266fbe024734
Murad501 <mm845635@gmail.com>
2023-02-17 17:44:19 +0600

    jobs for freshers and experienced section add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ExperiencedJob from './ExperiencedJob/ExperiencedJob';
 import FindJob from './FindJob/FindJob';
+import FresherJob from './FresherJob/FresherJob';
 
 const Home = () => {
     return (
         <div>
             <FindJob></FindJob>
+            <FresherJob></FresherJob>
+            <ExperiencedJob></ExperiencedJob>
         </div>
     );
 };
